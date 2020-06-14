28191baaad6beea03dd23600bf3ff338decdfc29
unknown <Administrador@FACISAA-NVM-003.NVM.UNEMAT>
2020-06-14 15:06:18 -0400

    pequenas correções
    Signed-off-by: unknown <Administrador@FACISAA-NVM-003.NVM.UNEMAT>

--- carol/lembrete.php
+++ carol/lembrete.php
@@ -3,8 +3,8 @@
 
 // Update the path below to your autoload.php,
 // see https://getcomposer.org/doc/01-basic-usage.md
-require_once './vendor/autoload.php';
-require_once './variaveis.php';
+require_once '../vendor/autoload.php';
+require_once '../variaveis.php';
 
 use Twilio\Rest\Client;
 
